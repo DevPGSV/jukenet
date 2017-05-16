6202fdc7d2e8fb64bf7a719d9470021a8bfc496e
DevPGSV <DevPGSV@gmail.com>
2017-05-16 20:25:17 +0200

    Separated received from sent messages

--- messages.php
+++ messages.php
@@ -46,32 +46,67 @@ $activeTab = 'messages';
           $messages = $db->getUserMessages($user['id']);
           // echo '<pre>', print_r($messages, true), '</pre>';
           ?>
-          <div class="table-responsive">
-            <table class="table table-striped table-bordered table-hover" style="table-layout:auto;">
-              <tr>
-                <th class="col-sm-1">De</th>
-                <th class="col-sm-1">Para</th>
-                <th class="col-sm-8">Asunto</th>
-                <th class="col-sm-2">Recibido</th>
-              </tr>
-              <?php
-              foreach ($messages as $message) {
-                $class='';
-                if (!$message['isRead']) {
-                  $class .= 'message_not_read';
-                }
-                echo "
-                <tr class='$class message_entry' data-messageid='{$message['id']}'>
-                  <td data-userid='{$message['from_user.id']}' data-action='popup_user'><span data-userid='{$message['from_user.id']}'>{$message['from_user.username']}</span></td>
-                  <td data-userid='{$message['to_user.id']}' data-action='popup_user'><span data-userid='{$message['to_user.id']}'>{$message['to_user.username']}</span></td>
-                  <td data-messageid='{$message['id']}' data-action='popup_message'>{$message['subject']}</td>
-                  <td>{$message['timestamp']}</td>
-                </tr>
-                ";
-              }
+          <ul class="nav nav-tabs">
+            <li class="active"><a data-toggle="tab" href="#messages_sent">Enviados</a></li>
+            <li><a data-toggle="tab" href="#messages_received">Recibidos</a></li>
+          </ul>
+          <div class="tab-content">
+            <div id="messages_sent" class="tab-pane fade in active">
+              <div class="table-responsive">
+                <table class="table table-striped table-bordered table-hover" style="table-layout:auto;">
+                  <tr>
+                    <th class="col-sm-1">Para</th>
+                    <th class="col-sm-8">Asunto</th>
+                    <th class="col-sm-2">Recibido</th>
+                  </tr>
+                  <?php
+                  foreach ($messages as $message) {
+                    if ($message['from_user.id'] === $user['id']) continue;
+                    $class='';
+                    if (!$message['isRead']) {
+                      $class .= 'message_not_read';
+                    }
+                    echo "
+                    <tr class='$class message_entry' data-messageid='{$message['id']}'>
+                      <td data-userid='{$message['to_user.id']}' data-action='popup_user'><span data-userid='{$message['to_user.id']}'>{$message['to_user.username']}</span></td>
+                      <td data-messageid='{$message['id']}' data-action='popup_message'>{$message['subject']}</td>
+                      <td>{$message['timestamp']}</td>
+                    </tr>
+                    ";
+                  }
 
-              ?>
-            </table>
+                  ?>
+                </table>
+              </div>
+            </div>
+            <div id="messages_received" class="tab-pane fade">
+              <div class="table-responsive">
+                <table class="table table-striped table-bordered table-hover" style="table-layout:auto;">
+                  <tr>
+                    <th class="col-sm-1">De</th>
+                    <th class="col-sm-8">Asunto</th>
+                    <th class="col-sm-2">Recibido</th>
+                  </tr>
+                  <?php
+                  foreach ($messages as $message) {
+                    if ($message['to_user.id'] === $user['id']) continue;
+                    $class='';
+                    if (!$message['isRead']) {
+                      $class .= 'message_not_read';
+                    }
+                    echo "
+                    <tr class='$class message_entry' data-messageid='{$message['id']}'>
+                      <td data-userid='{$message['from_user.id']}' data-action='popup_user'><span data-userid='{$message['from_user.id']}'>{$message['from_user.username']}</span></td>
+                      <td data-messageid='{$message['id']}' data-action='popup_message'>{$message['subject']}</td>
+                      <td>{$message['timestamp']}</td>
+                    </tr>
+                    ";
+                  }
+
+                  ?>
+                </table>
+              </div>
+            </div>
           </div>
           <?php
         endif;
