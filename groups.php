8d28bf5593493ef0f0217fc9e94a4a9379e17d35
DevPGSV <DevPGSV@gmail.com>
2017-05-17 07:57:17 +0200

    Added temporal groups page

---
+++ groups.php
@@ -0,0 +1,35 @@
+<?php
+require_once(__DIR__ . '/core/init.php');
+$pageTitle = 'Home';
+$activeTab = 'home';
+
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php
+  require_once('core/page-head.php');
+  ?>
+</head>
+</body>
+  <?php
+  require_once('core/page-navbar.php');
+  ?>
+
+  <div class="container-fluid" style="margin-top:50px">
+    <div class="row">
+      <div class="col-sm-12">
+        <?php
+        if ($user === false) :
+            echo 'Only for logged in users!';
+        else:
+          ?>
+          <p>Groups</p>
+          <?php
+        endif;
+        ?>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
