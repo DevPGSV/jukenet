f1e4499714e12cb92316dc52d15de8e089f2c86f
DevPGSV <DevPGSV@gmail.com>
2017-05-17 08:00:44 +0200

    Added bootstrap-select library and some basic temporal style

--- core/page-head.php
+++ core/page-head.php
@@ -10,4 +10,18 @@
 <link rel="stylesheet" href="libs/jquery-ui-1.12.1/jquery-ui.css">
 <script src="libs/jquery-ui-1.12.1/jquery-ui.js"></script>
 
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/css/bootstrap-select.min.css">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/bootstrap-select.min.js"></script>
+<!-- (Optional) Latest compiled and minified JavaScript translation files -->
+<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/i18n/defaults-*.min.js"></script>-->
+
+
 <script src="js/script.js"></script>
+
+<style>
+body.modal-open .ui-datepicker {
+    z-index: 1200 !important;
+}
+</style>
